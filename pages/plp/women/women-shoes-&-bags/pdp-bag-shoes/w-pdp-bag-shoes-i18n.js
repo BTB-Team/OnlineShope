278bf5f2fa28130eaf9138e7ae30da9bc17acbeb
Najiba <ebrahiminajiba43@gmail.com>
2026-07-13 11:46:44 +0430

    Implement bag and shoes languages codes

---
+++ pages/plp/women/women-shoes-&-bags/pdp-bag-shoes/w-pdp-bag-shoes-i18n.js
@@ -0,0 +1,235 @@
+export default {
+
+    fa: {
+
+
+        // Breadcrumb
+
+        bagsShoes:
+            "کیف و کفش",
+
+
+        products:
+            "محصولات",
+
+
+
+        productDetails:
+            "جزئیات محصول",
+
+
+
+
+        // Product
+
+        description:
+            "توضیحات",
+
+
+
+        specifications:
+            "مشخصات",
+
+
+
+        addToCart:
+            "افزودن به سبد خرید",
+
+
+
+        noSpecifications:
+            "مشخصاتی وجود ندارد",
+
+
+
+
+        // Cart
+
+        addedToCart:
+            "محصول به سبد خرید اضافه شد",
+
+
+
+        remove:
+            "حذف",
+
+
+
+        removedFromCart:
+            "محصول حذف شد",
+
+
+
+        yourCart:
+            "سبد خرید شما",
+
+
+
+        emptyCart:
+            "سبد خرید خالی است",
+
+
+
+        subtotal:
+            "جمع کل",
+
+
+
+        checkout:
+            "تسویه حساب با واتساپ",
+
+
+
+
+        // Product categories
+
+        sneakers:
+            "کتانی",
+
+
+        heels:
+            "کفش پاشنه‌دار",
+
+
+        boots:
+            "بوت",
+
+
+        sandals:
+            "صندل",
+
+
+        handbags:
+            "کیف دستی",
+
+
+        shoulderBags:
+            "کیف دوشی",
+
+
+        backpacks:
+            "کوله‌پشتی"
+
+
+    },
+
+
+
+
+
+
+    en: {
+
+
+        // Breadcrumb
+
+        bagsShoes:
+            "Bags & Shoes",
+
+
+        products:
+            "Products",
+
+
+
+        productDetails:
+            "Product Details",
+
+
+
+
+        // Product
+
+        description:
+            "Description",
+
+
+
+        specifications:
+            "Specifications",
+
+
+
+        addToCart:
+            "Add To Cart",
+
+
+
+        noSpecifications:
+            "No specifications available",
+
+
+
+
+        // Cart
+
+        addedToCart:
+            "Product added to cart",
+
+
+
+        remove:
+            "Remove",
+
+
+
+        removedFromCart:
+            "Product removed",
+
+
+
+        yourCart:
+            "Your Cart",
+
+
+
+        emptyCart:
+            "Your cart is empty",
+
+
+
+        subtotal:
+            "Subtotal",
+
+
+
+        checkout:
+            "Checkout via WhatsApp",
+
+
+
+
+        // Product categories
+
+        sneakers:
+            "Sneakers",
+
+
+        heels:
+            "High Heels",
+
+
+        boots:
+            "Boots",
+
+
+        sandals:
+            "Sandals",
+
+
+        handbags:
+            "Handbags",
+
+
+        shoulderBags:
+            "Shoulder Bags",
+
+
+        backpacks:
+            "Backpacks"
+
+
+    }
+
+
+};
