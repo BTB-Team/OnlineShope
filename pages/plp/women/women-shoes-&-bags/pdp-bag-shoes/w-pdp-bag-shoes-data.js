33e1eeed5717437afe9b14a443a0021d0afcc99d
Najiba <ebrahiminajiba43@gmail.com>
2026-07-13 11:42:34 +0430

    Implement bag and shoes data page

---
+++ pages/plp/women/women-shoes-&-bags/pdp-bag-shoes/w-pdp-bag-shoes-data.js
@@ -0,0 +1,659 @@
+export const bagShoesPdpData = {
+
+  1: {
+
+    brand: {
+      en: "Fashion Step",
+      fa: "فشن استپ",
+    },
+
+    images: [
+      "https://i.pinimg.com/1200x/a4/92/27/a49227b18ba0f449723883435d5db73e.jpg",
+      "https://i.pinimg.com/1200x/a4/92/27/a49227b18ba0f449723883435d5db73e.jpg",
+      "https://i.pinimg.com/1200x/a4/92/27/a49227b18ba0f449723883435d5db73e.jpg",
+    ],
+
+    specifications: [
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Sneakers",
+          fa:"کتانی"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Material",
+          fa:"جنس"
+        },
+
+        value:{
+          en:"Synthetic Leather",
+          fa:"چرم مصنوعی"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Usage",
+          fa:"کاربرد"
+        },
+
+        value:{
+          en:"Daily Wear",
+          fa:"استفاده روزمره"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  2: {
+
+    brand:{
+      en:"Fashion Step",
+      fa:"فشن استپ"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/1200x/e8/b5/a0/e8b5a073ce07818ee7791f29ca23bd03.jpg",
+      "https://i.pinimg.com/1200x/e8/b5/a0/e8b5a073ce07818ee7791f29ca23bd03.jpg",
+      "https://i.pinimg.com/1200x/e8/b5/a0/e8b5a073ce07818ee7791f29ca23bd03.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Sport Sneakers",
+          fa:"کتانی ورزشی"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Material",
+          fa:"جنس"
+        },
+
+        value:{
+          en:"Lightweight Fabric",
+          fa:"پارچه سبک"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  3:{
+
+
+    brand:{
+      en:"Elegant Style",
+      fa:"الگانس"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/736x/f3/c1/aa/f3c1aa5ca82f108cae02d7ef3266d12d.jpg",
+      "https://i.pinimg.com/736x/f3/c1/aa/f3c1aa5ca82f108cae02d7ef3266d12d.jpg",
+      "https://i.pinimg.com/736x/f3/c1/aa/f3c1aa5ca82f108cae02d7ef3266d12d.jpg"
+
+    ],
+
+
+    specifications:[
+
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"High Heels",
+          fa:"کفش پاشنه بلند"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Occasion",
+          fa:"مناسب برای"
+        },
+
+        value:{
+          en:"Formal Events",
+          fa:"مهمانی و مراسم"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  4:{
+
+
+    brand:{
+      en:"Elegant Style",
+      fa:"الگانس"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/1200x/f1/b3/07/f1b307a761104edd2a66ca8a2c34abd9.jpg",
+      "https://i.pinimg.com/1200x/f1/b3/07/f1b307a761104edd2a66ca8a2c34abd9.jpg",
+      "https://i.pinimg.com/1200x/f1/b3/07/f1b307a761104edd2a66ca8a2c34abd9.jpg"
+
+    ],
+
+
+    specifications:[
+
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Pointed Heels",
+          fa:"کفش نوک تیز"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Style",
+          fa:"استایل"
+        },
+
+        value:{
+          en:"Classic",
+          fa:"کلاسیک"
+        }
+      }
+
+    ]
+
+  },
+  5: {
+
+    brand:{
+      en:"Urban Boots",
+      fa:"اوربان بوت"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/736x/07/0c/94/070c94e1a64377d450842e2278d4e6f4.jpg",
+      "https://i.pinimg.com/736x/07/0c/94/070c94e1a64377d450842e2278d4e6f4.jpg",
+      "https://i.pinimg.com/736x/07/0c/94/070c94e1a64377d450842e2278d4e6f4.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Ankle Boots",
+          fa:"بوت کوتاه"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Material",
+          fa:"جنس"
+        },
+
+        value:{
+          en:"Leather",
+          fa:"چرم"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  6: {
+
+    brand:{
+      en:"Urban Boots",
+      fa:"اوربان بوت"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/736x/35/87/de/3587de1fc3b286266e5a65970f56ccae.jpg",
+      "https://i.pinimg.com/736x/35/87/de/3587de1fc3b286266e5a65970f56ccae.jpg",
+      "https://i.pinimg.com/736x/35/87/de/3587de1fc3b286266e5a65970f56ccae.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Long Boots",
+          fa:"بوت بلند"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Season",
+          fa:"فصل"
+        },
+
+        value:{
+          en:"Autumn & Winter",
+          fa:"پاییز و زمستان"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  7: {
+
+    brand:{
+      en:"Summer Style",
+      fa:"سامر استایل"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/1200x/fa/70/04/fa7004f36e760227cc00666ddd321ccd.jpg",
+      "https://i.pinimg.com/1200x/fa/70/04/fa7004f36e760227cc00666ddd321ccd.jpg",
+      "https://i.pinimg.com/1200x/fa/70/04/fa7004f36e760227cc00666ddd321ccd.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Flat Sandals",
+          fa:"صندل تخت"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Season",
+          fa:"فصل"
+        },
+
+        value:{
+          en:"Summer",
+          fa:"تابستان"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  8: {
+
+    brand:{
+      en:"Luxury Bags",
+      fa:"کیف لوکس"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/1200x/2f/95/00/2f950057872000ed992bbe8a40e03d8f.jpg",
+      "https://i.pinimg.com/1200x/2f/95/00/2f950057872000ed992bbe8a40e03d8f.jpg",
+      "https://i.pinimg.com/1200x/2f/95/00/2f950057872000ed992bbe8a40e03d8f.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Handbag",
+          fa:"کیف دستی"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Material",
+          fa:"جنس"
+        },
+
+        value:{
+          en:"Premium Leather",
+          fa:"چرم با کیفیت"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  9: {
+
+    brand:{
+      en:"Luxury Bags",
+      fa:"کیف لوکس"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/1200x/76/57/c3/7657c3e5bc31324f249bc92245eeda71.jpg",
+      "https://i.pinimg.com/1200x/76/57/c3/7657c3e5bc31324f249bc92245eeda71.jpg",
+      "https://i.pinimg.com/1200x/76/57/c3/7657c3e5bc31324f249bc92245eeda71.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Leather Bag",
+          fa:"کیف چرمی"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Style",
+          fa:"استایل"
+        },
+
+        value:{
+          en:"Classic",
+          fa:"کلاسیک"
+        }
+      }
+
+    ]
+
+},
+    10: {
+
+    brand:{
+      en:"Modern Bags",
+      fa:"کیف مدرن"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/736x/cf/7a/ca/cf7acab94abcd2f5c7cc15ecb46e5048.jpg",
+      "https://i.pinimg.com/736x/cf/7a/ca/cf7acab94abcd2f5c7cc15ecb46e5048.jpg",
+      "https://i.pinimg.com/736x/cf/7a/ca/cf7acab94abcd2f5c7cc15ecb46e5048.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Shoulder Bag",
+          fa:"کیف دوشی"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Usage",
+          fa:"کاربرد"
+        },
+
+        value:{
+          en:"Daily Use",
+          fa:"استفاده روزمره"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  11: {
+
+    brand:{
+      en:"Modern Bags",
+      fa:"کیف مدرن"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/1200x/8e/d3/88/8ed388cf74974982b9bdb280133a4ac0.jpg",
+      "https://i.pinimg.com/1200x/8e/d3/88/8ed388cf74974982b9bdb280133a4ac0.jpg",
+      "https://i.pinimg.com/1200x/8e/d3/88/8ed388cf74974982b9bdb280133a4ac0.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Minimal Shoulder Bag",
+          fa:"کیف دوشی مینیمال"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Style",
+          fa:"استایل"
+        },
+
+        value:{
+          en:"Modern",
+          fa:"مدرن"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  12: {
+
+    brand:{
+      en:"Travel Style",
+      fa:"تراول استایل"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/736x/07/a8/3b/07a83b5828b7e80918bb63f774aeae1b.jpg",
+      "https://i.pinimg.com/736x/07/a8/3b/07a83b5828b7e80918bb63f774aeae1b.jpg",
+      "https://i.pinimg.com/736x/07/a8/3b/07a83b5828b7e80918bb63f774aeae1b.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Backpack",
+          fa:"کوله پشتی"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Usage",
+          fa:"کاربرد"
+        },
+
+        value:{
+          en:"Travel & Daily",
+          fa:"سفر و روزمره"
+        }
+      }
+
+    ]
+
+  },
+
+
+
+  13: {
+
+    brand:{
+      en:"Travel Style",
+      fa:"تراول استایل"
+    },
+
+
+    images:[
+
+      "https://i.pinimg.com/736x/01/32/63/013263a013e3f6dcaf23987bcd1bbe86.jpg",
+      "https://i.pinimg.com/736x/01/32/63/013263a013e3f6dcaf23987bcd1bbe86.jpg",
+      "https://i.pinimg.com/736x/01/32/63/013263a013e3f6dcaf23987bcd1bbe86.jpg"
+
+    ],
+
+
+    specifications:[
+
+      {
+        name:{
+          en:"Type",
+          fa:"نوع"
+        },
+
+        value:{
+          en:"Fashion Backpack",
+          fa:"کوله پشتی فشن"
+        }
+      },
+
+
+      {
+        name:{
+          en:"Material",
+          fa:"جنس"
+        },
+
+        value:{
+          en:"Fabric",
+          fa:"پارچه"
+        }
+      }
+
+    ]
+
+  }
+
+
+};
