ae3e8624a72a36cd42c3ffce4fd0b0feec2baaac
SetaraHaidari <haidarisetara4@gmail.com>
2026-08-16 22:49:16 +0430

    Add women shoes and bags shop

---
+++ pages/plp/women/women-shoes-&-bags/data.js
@@ -0,0 +1,42 @@
+export const categories = [
+  {
+    id: 1,
+    name: {
+      en: "Sneakers",
+      fa: "کتانی"
+    },
+    image: "./images/sneaker4.jpg",
+    productCount: 24,
+    link: "./sneakers/sneakers.html",
+  },
+  {
+    id: 2,
+    name: {
+      en: "Heels",
+      fa: "کفش پاشنه‌بلند"
+    },
+    image: "./images/heel8.jpg",
+    productCount: 18,
+    link: "./heels/heels.html",
+  },
+  {
+    id: 3,
+    name: {
+      en: "Boots",
+      fa: "بوت"
+    },
+    image: "./images/boot.jpg",
+    productCount: 12,
+    link: "./boots/boots.html",
+  },
+  {
+    id: 4,
+    name: {
+      en: "Sandals",
+      fa: "صندل"
+    },
+    image: "./images/sandal7.jpg",
+    productCount: 31,
+    link: "./sandals/sandals.html",
+  },
+];
---
+++ pages/plp/women/women-shoes-&-bags/i18n.js
@@ -0,0 +1,19 @@
+export const i18n = {
+  en: {
+    pageTitle: "Women's Shoes & Bags",
+    pageDescription: "Discover our latest collection.",
+    searchPlaceholder: "Search products...",
+    breadcrumbHome: "Home",
+    breadcrumbWomen: "Women",
+    breadcrumbCollection: "Shoes & Bags",
+  },
+
+  fa: {
+    pageTitle: "کیف و کفش زنانه",
+    pageDescription: "جدیدترین مجموعه کیف و کفش زنانه را کشف کنید.",
+    searchPlaceholder: "جستجوی محصولات...",
+    breadcrumbHome: "خانه",
+    breadcrumbWomen: "زنانه",
+    breadcrumbCollection: "کیف و کفش",
+  },
+};
